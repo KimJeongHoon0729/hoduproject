0fb374129bd3f2ee795511488b5fb5b4224f9c2b
orinaldaramg <duck729039@gmail.com>
2023-03-31 10:09:23 +0900

    업로드 중간 점검

--- hoduproject/hodu/src/main/java/com/woori/hodu/FileController.java
+++ hoduproject/hodu/src/main/java/com/woori/hodu/FileController.java
@@ -5,16 +5,15 @@ import java.io.File;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
 @Controller
 public class FileController {
 	
-	@GetMapping("/uplaod")
+	@GetMapping("/uploadform")
 	public String upload() {
-		return "upload";
+		return "uploadform";
 	}
 	
 	@PostMapping("/upload")
@@ -36,8 +35,6 @@ public class FileController {
 			}
 			
 		}
-		
-
 		return "uploadok";
 	}
 	
